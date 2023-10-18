ac2aaa0fb074d5b25eaa6271ae57a2eead12794c
daorichard <98173508+daorichard@users.noreply.github.com>
2023-10-18 19:31:11 -0400

    added routing for pages and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import Product from './pages/Product/Product';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+const Layout = () => {
+  return (
+    <div className='classname'>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <span>Home</span>,
-  },
-  {
-    path: '/products/:id',
-    element: <span>Category</span>,
-  },
-  {
-    path: '/product/:id',
-    element: <span>Product</span>,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/products/:id',
+        element: <Products />,
+      },
+      {
+        path: '/product/:id',
+        element: <Product />,
+      },
+    ],
   },
 ]);
+
 function App() {
   return (
     <div>
